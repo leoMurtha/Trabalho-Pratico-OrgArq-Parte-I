85a622c4e9d7df26d558016ee6d5d163ce5c48a6
leoMurtha <aka.murtha@gmail.com>
2017-05-08 18:44:18 -0300

    Update rff.h

--- rff.h
+++ rff.h
@@ -3,21 +3,6 @@
 
 typedef struct rff RFF;
 
-/*
-	Write sequence on the output file:
-	Fixed Fields: ticket->doc->initialTime->updatedTime
-	Variable Fields: domain,name,city,state
-*/
-
-/* Funtion that free's the memory of a register content */
-void freeRegisterContent(RFF *);
-
-/* Function that will print only one register */
-void printRegister_RFF(RFF ,int );
-
-/* Function that read's and recovers one register of the given file */ 
-RFF readOneBinary_RFF(FILE *);
-
 /* Print an register that match the position and the criteria given by the user */ 
 void printViaPosField_RFF(char *,int ,int );
 
@@ -29,10 +14,6 @@ void printViaField_RFF(char *,int , char *);
 
 /* Function that print all the registers that were recovered one by one and by the demand of the user */
 void printAll_RFF(char *);
-
-/* Read's a register of type RFF(Fixed number of fields) of the input,
-   write's it in the output */
-void readAndWrite_RFF(FILE *,FILE *);
 
 /* Receives a name of a file read's it and create's a new file using
    fixed number of fields in a variable size register */
