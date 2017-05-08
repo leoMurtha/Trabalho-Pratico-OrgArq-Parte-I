3898f27e91e2fe768f903977beb9ce64c3c821e9
leoMurtha <aka.murtha@gmail.com>
2017-05-08 18:47:13 -0300

    Update rsi.h

--- rsi.h
+++ rsi.h
@@ -7,14 +7,12 @@
 /* Receives the entry file(.csv) name and return the output file(data file) name */
 char *createNewFile_RSI(char *);
 /* Receives the file name and prints all the registers */
-void printAllData_RSI(char *);
+void printAll_RSI(char *);
 /* Receives the file name, the field position and the field searched value and prints all the searched registers*/
-void printDeterminedRegisters_RSI(char *,int,char *);
+void printViaField_RSI(char *,int,char *);
 /* Receives the register position */
-void printOneRegister_RSI(char *,int );
+void printViaPosition_RSI(char *,int );
 /* Receives the file name, the register position and the field position */
-void printField_RSI(char *,int,int);
-
-
+void printViaPosField_RSI(char *,int,int);
 
 #endif
